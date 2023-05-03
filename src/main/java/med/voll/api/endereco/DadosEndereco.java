442a479dd543b3c5f259ba64294a0d16659b80af
Vitorsandrade <vitorsantos.dandrade@gmail.com>
2023-05-03 19:11:34 -0300

    tabelas

--- src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -1,4 +1,20 @@
 package med.voll.api.endereco;
 
-public record DadosEndereco (String cep, String logradouro, String cidade, String bairro, String numero, String uf, String complemento) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosEndereco(
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
+        String cep,
+        @NotBlank
+        String logradouro,
+        @NotBlank
+        String cidade,
+        @NotBlank
+        String bairro,
+        String numero,
+        @NotBlank
+        String uf,
+        String complemento) {
 }
